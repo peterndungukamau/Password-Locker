df91dfef6ac917372ccd684e81c658d50ad510f5
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-10 18:44:48 +0300

    add user class for user details

--- credentials.py
+++ credentials.py
@@ -2,7 +2,7 @@ class Credentials:
     """
     Class that generates new instances of credentials
     """
-    def __init__(self,account_name,password):
+    def __init__(self, account_name, password):
 
         self.account_name = account_name
         self.password = password
--- user.py
+++ user.py
@@ -1,6 +1,9 @@
 class User:
 
-    def __init__(self, username):
+    def __init__(self, first_name, last_name, username):
+
         self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
 
 
