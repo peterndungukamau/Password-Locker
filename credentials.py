df7a8d20d3d07773d0ab84e3def886b95d82ea81
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-10 18:25:43 +0300

    add credentials class for name and passwords

---
+++ credentials.py
@@ -0,0 +1,8 @@
+class Credentials:
+    """
+    Class that generates new instances of credentials
+    """
+    def __init__(self,account_name,password):
+
+        self.account_name = account_name
+        self.password = password
