82f3b59463ee78bb5f186fe1e6e491ed3e3223fe
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-10 22:53:43 +0300

    add input to capture username and print

---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.6
+print("Hello! Welcome to Password Locker app where passwords are found")
+print("Do you own an Account?If Yes kindly enter your username below")
+user_name = input()
+
+print(f"Welcome {user_name} How may i help you?")
+
+
+
